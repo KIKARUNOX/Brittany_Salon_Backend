115a7fb17d16d39dc4a2e59db2bbf7ebfcfce5ab
Isaac Chevez <isaac.chevez@ucr.ac.cr>
2026-01-27 17:01:06 -0600

    entidad cita creada

---
+++ Brittany_Salon_Backend/Domain/Entities/Appointment.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Brittany_Salon_Backend.Domain.Entities
+{
+    [Table("Appointment")]
+    public class Appointment
+    {
+        [Key]
+        [Column("appointmentId")]
+        public int AppointmentId { get; set; }
+
+        [Required]
+        [Column("appointmentDate", TypeName = "date")]
+        public DateTime AppointmentDate { get; set; }
+
+        [Required]
+        [Column("startTime")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Column("endTime")]
+        public DateTime EndTime { get; set; }
+
+        [MaxLength(50)]
+        [Column("appointmentStatus")]
+        public string? AppointmentStatus { get; set; }
+
+        [Column("totalCost", TypeName = "decimal(10,2)")]
+        public decimal? TotalCost { get; set; }
+
+        [Column("isActive")]
+        public bool IsActive { get; set; } = true;
+
+        [Required]
+        [Column("clientId")]
+        public int ClientId { get; set; }
+
+        // Navegación
+        [ForeignKey(nameof(ClientId))]
+        public Clients Client { get; set; } = null!;
+    }
+}
