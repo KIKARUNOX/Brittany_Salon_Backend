9fb70b1e606cff218450e0bc9c5417bbaa5c1c14
Isaac Chevez <isaac.chevez@ucr.ac.cr>
2026-01-29 21:24:34 -0600

    arreglo de error en la entidad Client

--- Brittany_Salon_Backend/Domain/Entities/Clients.cs
+++ Brittany_Salon_Backend/Domain/Entities/Clients.cs
@@ -1,6 +1,13 @@
-﻿namespace Brittany_Salon_Backend.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Brittany_Salon_Backend.Domain.Entities
 {
+    [Table("Client")]
     public class Clients
     {
+        [Key]
+        [Column("clientId")]
+        public int ClientId { get; set; }
     }
 }
