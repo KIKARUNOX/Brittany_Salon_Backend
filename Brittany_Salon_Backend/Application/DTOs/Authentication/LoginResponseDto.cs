7a3184e1fb2276a0de2176f4ce31fbe470e1cdc0
britany.villalobos <britany.villalobos@ucr.ac.cr>
2026-01-31 21:00:47 -0600

    login sencillo

---
+++ Brittany_Salon_Backend/Application/DTOs/Authentication/LoginResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Brittany_Salon_Backend.Application.DTOs.Authentication
+{
+    public class LoginResponseDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;  // "EMPLOYEE" "CLIENT"
+        public string Token { get; set; } = string.Empty;  //JWT
+    }
+}
